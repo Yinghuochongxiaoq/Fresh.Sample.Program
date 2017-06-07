c0a6da4c4a39f00aacc5e01a76db1e88a774e8ed
FreshMan <qinbocai@sina.cn>
2017-06-07 18:01:19 +0800

    Init Program.

---
+++ src/Fresh.Sample.DataAccess/Common/DataTableGlobal.cs
@@ -0,0 +1,13 @@
+﻿namespace Fresh.Sample.DataAccess.Common
+{
+    /// <summary>
+    /// DataTable global
+    /// </summary>
+    public static class DataTableGlobal
+    {
+        /// <summary>
+        /// PiFErrorLog table
+        /// </summary>
+        public static string PiFErrorLog = "PiFErrorLog";
+    }
+}
---
+++ src/Fresh.Sample.Program/Startup.cs
@@ -0,0 +1,76 @@
+﻿using Fresh.Sample.Business.LogHistoryBll;
+using Fresh.Sample.Interface.ILogHistoryBll;
+using Fresh.Sample.Program.MiddleWares;
+using FreshCommonUtility.CoreModel;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+
+namespace Fresh.Sample.Program
+{
+    public class Startup
+    {
+        /// <summary>
+        /// construct function
+        /// </summary>
+        /// <param name="env"></param>
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, true);
+
+            if (env.IsEnvironment("Development"))
+            {
+                // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
+                builder.AddApplicationInsightsSettings(true);
+            }
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        /// <summary>
+        /// interface server
+        /// </summary>
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddApplicationInsightsTelemetry(Configuration);
+
+            services.AddMvc(opt =>
+            {
+                opt.UseControllRoutePrefix(new RouteAttribute("api/"));
+            });
+            services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            //Add options
+            services.AddOptions();
+            services.Configure<AppSettingsModel>(Configuration.GetSection("AppSettings"));
+            //Add Error log server
+            services.AddTransient<IErrorLogBll, ErrorLogBll>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+            app.UseApplicationInsightsRequestTelemetry();
+
+            app.UseApplicationInsightsExceptionTelemetry();
+            //异常处理中间件
+            app.UseMiddleware(typeof(ExceptionHandlerMiddleWare));
+            app.UseMvc();
+        }
+    }
+}
